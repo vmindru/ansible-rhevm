9a6869194ce0ebf2712da6a6e1aec5b5d6a497dd
Veaceslav Mindru <mindruv@gmail.com>
2015-12-19 22:57:40 +0100

    .

---
+++ files/get_vm.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+
+import libvirt
+import sys
+from optparse import OptionParser
+
+
+def get_opt():
+    parser = OptionParser("usage: %prog -u USER -H HOST -K path_key_file")
+    parser.add_option(  "-u",
+                        "--user",
+                        dest="VirtUser",
+                        help="QEMU User to connect",
+                        metavar="USER")
+    parser.add_option(  "-H",
+                        "--host",
+                        dest="VirtHost",
+                        help="QEMU HOST to connect",
+                        metavar="HOST")
+    parser.add_option(  "-k",
+                        "--key",
+                        dest="SshKey",
+                        help="PATH to key for SSH, if not specified default will be used",
+                        metavar="KEY")
+    parser.add_option(  "-l",
+                        "--local",
+                        dest="Local",
+                        action="store_true",
+                        help="query local qemu:///system",
+                        )
+    (options, args) = parser.parse_args()
+    if not options.Local:
+        if not options.VirtUser:
+            parser.error("you have to specify -u USER" )
+        if not options.VirtHost:
+            parser.error("you have to specify -H host" )
+        if not options.SshKey:
+            parser.error("you have to specify -k key" )
+    return options,args,parser
+
+def getcount(VirtUri):
+    conn = libvirt.open(VirtUri)
+    Count = conn.numOfDomains()
+    return Count
+
+if __name__ == "__main__":
+    (opts, args, parser) = get_opt()
+    if not opts.Local:
+        VirtUser=opts.VirtUser
+        VirtHost=opts.VirtHost
+        SshKey=opts.SshKey
+        VirtUri = "qemu+ssh://"+VirtUser+"@"+VirtHost+"/system"+"?keyfile="+SshKey
+        print getcount(VirtUri)
+    else:
+        VirtUri = "qemu:///system"
+        print getcount(VirtUri)
+
